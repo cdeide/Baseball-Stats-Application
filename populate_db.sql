e84423821ab2d98ec164b9124aa8ae195beb0c4c
Connor Deide <cdeide@zagmail.gonzaga.edu>
2022-11-20 18:59:40 -0800

    Update

---
+++ populate_db.sql
@@ -0,0 +1,12 @@
+/****************************************************************************
+* NAME: Connor Deide
+* CLASS: CPSC 321 Section 2
+* DATE: 11/22/22
+* HOMEWORK: HW6
+* DESCRIPTION:
+****************************************************************************/
+
+INSERT INTO Team VALUES
+    ("Seattle Mariners", "0C2C56", "005C5C", "Scott Servais", 1977,
+    "The M's", "T-Mobile Park", 0, 0, 3,
+    "Based in Seattle, the Mariners")
